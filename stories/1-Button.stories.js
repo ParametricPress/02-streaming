d5aed55179eb9a350c318f109bedeed83097fc2f
Halden Lin <halden.lin@gmail.com>
2020-08-03 21:05:59 -0700

    Add VL story.

--- stories/1-Button.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CustomComponent from '../components/custom-component';
-
-export default {
-  title: 'Custom Component'
-};
-
-export const Custom = () => < CustomComponent />

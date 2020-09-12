ff0b330db2fc033150f9f2dedd2e239f5478a08f
Halden Lin <halden.lin@gmail.com>
2020-09-12 14:13:31 -0700

    Switch media picker to dark mode colors (#5)

--- components/constants.js
+++ components/constants.js
@@ -1,7 +1,8 @@
-export const markColor = '#4F4F4F';  // dark grey
-export const secondaryMarkColor = '#cfcfcf';  // lighter grey
+export const markColor = '#CCCCCC';  // white
+export const secondaryMarkColor = '#666666';  // lighter grey
 export const accentColor = '#d1ff99';  // parametric green
-export const darkAccentColor = '#82e010'  // darker paremtric green
-export const labelColor = '#AAAAAA';
-export const guideColor = '#E8E8E8';
+export const darkAccentColor = 'transparent'  // darker paremtric green
+export const labelColor = '#444444';
+export const guideColor = '#333333';
+export const textColor = '#EEEEEE';
 export const backgroundColor = '#222222';
